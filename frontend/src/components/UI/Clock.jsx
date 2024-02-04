7e5e0b1fc5025dd3dcd63474358a717950c5d244
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2024-02-05 00:42:50 +0800

    Update Clock.jsx

--- frontend/src/components/UI/Clock.jsx
+++ frontend/src/components/UI/Clock.jsx
@@ -12,7 +12,7 @@ const Clock = () => {
 
   useEffect(() => {
     const countDown = () => {
-      const destination = new Date("Feb 22, 2024").getTime();
+      const destination = new Date("Feb 23, 2024").getTime();
       interval = setInterval(() => {
         const now = new Date().getTime();
         const difference = destination - now;
