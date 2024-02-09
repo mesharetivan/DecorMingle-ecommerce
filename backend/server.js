ba422595ca6f19d52a5e9f56db83d11cf4473e7d
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2024-02-09 14:43:31 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,7 @@ const paymentsDatabase = {};
 // Use cors middleware
 app.use(
   cors({
-    origin:
-      "https://decor-mingle-ecommerce-9jq52h6n0-mesharet-ivan-loricas-projects.vercel.app",
+    origin: "https://decor-mingle-ecommerce.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
@@ -88,11 +87,10 @@ import("./decormingle-b79a5-firebase-adminsdk-ict07-9018a8fe95.json", {
           },
         ],
         application_context: {
-          return_url: `https://decor-mingle-ecommerce-9jq52h6n0-mesharet-ivan-loricas-projects.vercel.app/thankyou?orderID=${encodeURIComponent(
+          return_url: `https://decor-mingle-ecommerce.vercel.app/thankyou?orderID=${encodeURIComponent(
             orderID
           )}`,
-          cancel_url:
-            "https://decor-mingle-ecommerce-9jq52h6n0-mesharet-ivan-loricas-projects.vercel.app/home",
+          cancel_url: "https://decor-mingle-ecommerce.vercel.app/home",
         },
       });
 
