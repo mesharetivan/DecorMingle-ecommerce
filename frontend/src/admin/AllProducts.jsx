d41360996740034d41fe751d987613408ec6c61a
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2024-02-10 21:56:21 +0800

    Update

--- frontend/src/admin/AllProducts.jsx
+++ frontend/src/admin/AllProducts.jsx
@@ -12,6 +12,8 @@ import defaultProductImg from "../assets/images/arm-chair-01.jpg";
 import "../styles/all-products.css";
 
 import HomeLoader from "../components/Loader/HomeLoader";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
 
 const AllProducts = () => {
   const { data: products, loading } = useGetData("products");
@@ -32,69 +34,72 @@ const AllProducts = () => {
   };
 
   return (
-    <section>
-      <Container>
-        <Row>
-          <Col lg="12">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th className="products__th">Image</th>
-                  <th className="products__th">Title</th>
-                  <th className="products__th">Category</th>
-                  <th className="products__th">Price</th>
-                  <th className="products__th-action">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {loading ? (
+    <Helmet title="All Products">
+      <CommonSection title="All Products" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <table className="table">
+                <thead>
                   <tr>
-                    <td colSpan="5" style={{ textAlign: "center" }}>
-                      <div className="d-flex justify-content-center align-items-center">
-                        <HomeLoader />
-                      </div>
-                    </td>
+                    <th className="products__th">Image</th>
+                    <th className="products__th">Title</th>
+                    <th className="products__th">Category</th>
+                    <th className="products__th">Price</th>
+                    <th className="products__th-action">Action</th>
                   </tr>
-                ) : products.length > 0 ? (
-                  products.map((product) => (
-                    <tr key={product.id}>
-                      <td className="products__td">
-                        <img
-                          src={product.imgUrl || defaultProductImg}
-                          alt={product.productName}
-                          style={{ width: "50px", borderRadius: "8px" }}
-                        />
-                      </td>
-                      <td className="products__td">{product.productName}</td>
-                      <td className="products__td">{product.category}</td>
-                      <td className="products__td-price">₱{product.price}</td>
-                      <td className="products__td-delete">
-                        <button
-                          className="btn btn-danger"
-                          onClick={() => deleteProduct(product.id)}
-                          disabled={isDeleting}
-                        >
-                          {isDeleting ? "Deleting..." : "Delete"}
-                        </button>
+                </thead>
+                <tbody>
+                  {loading ? (
+                    <tr>
+                      <td colSpan="5" style={{ textAlign: "center" }}>
+                        <div className="d-flex justify-content-center align-items-center">
+                          <HomeLoader />
+                        </div>
                       </td>
                     </tr>
-                  ))
-                ) : (
-                  <tr>
-                    <td colSpan="5" className="text-center">
-                      No products available.
-                    </td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-            <Link to="/dashboard/add-product">
-              <button className="buy__btn">Add Product</button>
-            </Link>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+                  ) : products.length > 0 ? (
+                    products.map((product) => (
+                      <tr key={product.id}>
+                        <td className="products__td">
+                          <img
+                            src={product.imgUrl || defaultProductImg}
+                            alt={product.productName}
+                            style={{ width: "50px", borderRadius: "8px" }}
+                          />
+                        </td>
+                        <td className="products__td">{product.productName}</td>
+                        <td className="products__td">{product.category}</td>
+                        <td className="products__td-price">₱{product.price}</td>
+                        <td className="products__td-delete">
+                          <button
+                            className="btn btn-danger"
+                            onClick={() => deleteProduct(product.id)}
+                            disabled={isDeleting}
+                          >
+                            {isDeleting ? "Deleting..." : "Delete"}
+                          </button>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="5" className="text-center">
+                        No products available.
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+              <Link to="/dashboard/add-product">
+                <button className="buy__btn">Add Product</button>
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   );
 };
 
--- frontend/src/admin/Shipping.jsx
+++ frontend/src/admin/Shipping.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+import { Container } from "reactstrap";
 
 const Shipping = () => {
-  return <div>Shipping</div>;
+  return (
+    <Helmet title="Shipping Information">
+      <CommonSection title="Shipping Informations" />
+      <section>
+        <Container>
+          <h5>Shipping</h5>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Shipping;
