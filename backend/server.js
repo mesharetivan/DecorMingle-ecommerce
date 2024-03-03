efe7a332db62b556cb0e6758a4702c2b96f9fe1c
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2024-03-03 15:52:23 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -199,6 +199,7 @@ import("./decormingle-b79a5-firebase-adminsdk-ict07-9018a8fe95.json", {
       }
     });
 
+    // Define other routes and endpoints here...
 
     // Start the server
     app.listen(port, () => {
