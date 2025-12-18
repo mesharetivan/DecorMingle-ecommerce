aab9c8efdc58923e6deba5e0b169bcc2e242f3ab
mesharetivan <mesharetivan@gmail.com>
2025-12-19 03:17:30 +0800

    Revert "Update"
    This reverts commit 07b12b47656e609054971aa854ff42af9a5c7972.

--- frontend/src/firebase.config.js
+++ frontend/src/firebase.config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeooQ92rmAvRd6_tFYow8nfdopz3aYGzQ",
-  authDomain: "printamom.firebaseapp.com",
-  projectId: "printamom",
-  storageBucket: "printamom.firebasestorage.app",
-  messagingSenderId: "8660568974",
-  appId: "1:8660568974:web:0bce3ad92cfccd122172df",
-  measurementId: "G-K4EWVMKM1W",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
